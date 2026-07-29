f397c25569544dfad9e8bc16e5fbdf878013b2bb
Anusha Azmeera <anushaazmeera006@gmail.com>
2026-07-30 00:21:58 +0530

    Time: 311 ms (5.38%), Space: 81.5 MB (5.61%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,51 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n=s.length();
+//          string ans="";
+//         int  l =0;
+//         int r =0;
+//         unordered_set<char>st;
+//          while(r<n){
+//             while(st.find(s[r])!=st.end()){
+//                st.erase(s[l]);
+//                l++;
+               
+//             }
+//              st.insert(s[r]);
+//              ans = max(ans,r-l+1);
+//              r++;
+
+            
+            
+//          }
+//          return ans;
+//     }
+// };
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+
+        int l = 0;
+        int r = 0;
+        int ans = 0;
+
+        unordered_set<char> st;
+
+        while (r < n) {
+
+            while (st.find(s[r]) != st.end()) {
+                st.erase(s[l]);
+                l++;
+            }
+
+            st.insert(s[r]);
+            ans = max(ans, r - l + 1);
+
+            r++;
+        }
+
+        return ans;
+    }
+};
