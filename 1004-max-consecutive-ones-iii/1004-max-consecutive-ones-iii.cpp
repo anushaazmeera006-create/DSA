f74e031d6cd82109b1a9b907feb8ec384e654cd0
Anusha Azmeera <anushaazmeera006@gmail.com>
2026-07-30 19:31:07 +0530

    Time: 0 ms (100%), Space: 69.5 MB (95.15%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,28 +1,57 @@
+// // class Solution {
+// // public:
+// //     int longestOnes(vector<int>& nums, int k) {
+// //         int sum =0;
+// //         while(k!=0){
+// //            for(int i =0;i<nums.size();i++){
+                
+            
+// //                 if(nums[i]==0){
+// //                     len=len+1;
+// //                     k--;
+// //                 }
+// //                 else{
+// //                     len = len+nums[i];
+// //                 }
+// //             }
+            
+
+// //            }
+// //     }
+// // };
 // class Solution {
 // public:
 //     int longestOnes(vector<int>& nums, int k) {
-//         int sum =0;
-//         while(k!=0){
-//            for(int i =0;i<nums.size();i++){
-                
-            
-//                 if(nums[i]==0){
-//                     len=len+1;
-//                     k--;
-//                 }
-//                 else{
-//                     len = len+nums[i];
-//                 }
-//             }
-            
+//        int zero = 0;
+//        int l =0;
+//        int r =0;
+//        int maxlen =0;
+//        int n = nums.size();
+//        while(r<n){
+//         if(nums[r]==0){
+//             zero++;
 
-//            }
+//         }
+//             if(zero>k){
+//                 if(nums[l]==0){
+//                     zero--;
+//                 }
+//                 l++;
+//             }
+//             if(zero<=k){
+            
+//             int len = r-l+1;
+//           maxlen = max(maxlen,len);
+//             }
+//             r++;
+//        } 
+//        return maxlen;
 //     }
 // };
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-       int zero = 0;
+         int zero = 0;
        int l =0;
        int r =0;
        int maxlen =0;
@@ -32,7 +61,7 @@ public:
             zero++;
 
         }
-            if(zero>k){
+            while(zero>k){
                 if(nums[l]==0){
                     zero--;
                 }
@@ -45,7 +74,9 @@ public:
             }
             r++;
        } 
-       return maxlen;
+       return maxlen;   
+
     }
 };
+
 
