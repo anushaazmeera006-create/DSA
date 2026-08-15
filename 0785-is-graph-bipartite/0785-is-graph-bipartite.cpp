ab10e6894d9f4d072ccb878a0f5783985bc0f1c0
Anusha Azmeera <anushaazmeera006@gmail.com>
2026-08-15 16:16:22 +0530

    Time: 0 ms (100%), Space: 17.6 MB (39.51%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,73 @@
+// class Solution {
+// public:
+//     bool isBipartite(vector<vector<int>>& graph) {
+   
+//         int n=graph.size();
+//         queue<int>q;
+      
+//          vector<int> color(n,-1);
+//         vector<int> visi(n,0);
+//         for(int i = 0; i < n; i++) {
+//            if(color[i] == -1) {
+//              visi[i]=1;
+//              q.push(i);
+//             color[i]=0;
+//         while(!q.size()){
+//             int node=q.front();
+//             q.pop();
+ 
+//             for(auto it:graph(node)){
+//                 if(!visi[it]){
+//                     q.push(it);
+//                 visi[it]=1
+//                 color[it]=!(color[node]);   
+//                 }
+//                 else if(color[it]==color[node]){
+//                     return false;
+//                 }
+
+//             }
+            
+           
+//              }
+//                  }
+              
+     
+//        return false; 
+//     }
+// };
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1);
+        vector<int> visi(n, 0);
+        queue<int> q;
+
+        for(int i = 0; i < n; i++) {
+            if(visi[i]) continue;
+
+            q.push(i);
+            visi[i] = 1;
+            color[i] = 0;
+
+            while(!q.empty()) {
+                int node = q.front();
+                q.pop();
+
+                for(auto it : graph[node]) {
+                    if(!visi[it]) {
+                        visi[it] = 1;
+                        color[it] = !color[node];
+                        q.push(it);
+                    }
+                    else if(color[it] == color[node]) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
