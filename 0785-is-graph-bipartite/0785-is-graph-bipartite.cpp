8f881403e4f5020cf8f9628163c628423278e94a
Anusha Azmeera <anushaazmeera006@gmail.com>
2026-08-15 16:54:11 +0530

    Time: 0 ms (100%), Space: 17.3 MB (93.15%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,70 +1,109 @@
-// class Solution {
-// public:
-//     bool isBipartite(vector<vector<int>>& graph) {
+// // class Solution {
+// // public:
+// //     bool isBipartite(vector<vector<int>>& graph) {
    
-//         int n=graph.size();
-//         queue<int>q;
+// //         int n=graph.size();
+// //         queue<int>q;
       
-//          vector<int> color(n,-1);
-//         vector<int> visi(n,0);
-//         for(int i = 0; i < n; i++) {
-//            if(color[i] == -1) {
-//              visi[i]=1;
-//              q.push(i);
-//             color[i]=0;
-//         while(!q.size()){
-//             int node=q.front();
-//             q.pop();
+// //          vector<int> color(n,-1);
+// //         vector<int> visi(n,0);
+// //         for(int i = 0; i < n; i++) {
+// //            if(color[i] == -1) {
+// //              visi[i]=1;
+// //              q.push(i);
+// //             color[i]=0;
+// //         while(!q.size()){
+// //             int node=q.front();
+// //             q.pop();
  
-//             for(auto it:graph(node)){
-//                 if(!visi[it]){
-//                     q.push(it);
-//                 visi[it]=1
-//                 color[it]=!(color[node]);   
-//                 }
-//                 else if(color[it]==color[node]){
-//                     return false;
-//                 }
+// //             for(auto it:graph(node)){
+// //                 if(!visi[it]){
+// //                     q.push(it);
+// //                 visi[it]=1
+// //                 color[it]=!(color[node]);   
+// //                 }
+// //                 else if(color[it]==color[node]){
+// //                     return false;
+// //                 }
 
-//             }
+// //             }
             
            
-//              }
-//                  }
+// //              }
+// //                  }
               
      
-//        return false; 
+// //        return false; 
+// //     }
+// // };
+// class Solution {
+//     // using BFS
+// public:
+//     bool isBipartite(vector<vector<int>>& graph) {
+//         int n = graph.size();
+//         vector<int> color(n, -1);
+//         vector<int> visi(n, 0);
+//         queue<int> q;
+
+//         for(int i = 0; i < n; i++) {
+//             if(visi[i]) continue;
+
+//             q.push(i);
+//             visi[i] = 1;
+//             color[i] = 0;
+
+//             while(!q.empty()) {
+//                 int node = q.front();
+//                 q.pop();
+
+//                 for(auto it : graph[node]) {
+//                     if(!visi[it]) {
+//                         visi[it] = 1;
+//                         color[it] = !color[node];
+//                         q.push(it);
+//                     }
+//                     else if(color[it] == color[node]) {
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+
+//         return true;
 //     }
 // };
-class Solution {
+ class Solution {
 public:
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& visi, vector<int>& color) {
+        visi[node] = 1;
+
+        for(auto it : graph[node]) {
+            if(!visi[it]) {
+                color[it] = !color[node];
+
+                if(!dfs(it, graph, visi, color))
+                    return false;
+            }
+            else if(color[it] == color[node]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> color(n, -1);
+
         vector<int> visi(n, 0);
-        queue<int> q;
+        vector<int> color(n, -1);
 
         for(int i = 0; i < n; i++) {
-            if(visi[i]) continue;
+            if(!visi[i]) {
+                color[i] = 0;
 
-            q.push(i);
-            visi[i] = 1;
-            color[i] = 0;
-
-            while(!q.empty()) {
-                int node = q.front();
-                q.pop();
-
-                for(auto it : graph[node]) {
-                    if(!visi[it]) {
-                        visi[it] = 1;
-                        color[it] = !color[node];
-                        q.push(it);
-                    }
-                    else if(color[it] == color[node]) {
-                        return false;
-                    }
-                }
+                if(!dfs(i, graph, visi, color))
+                    return false;
             }
         }
 
