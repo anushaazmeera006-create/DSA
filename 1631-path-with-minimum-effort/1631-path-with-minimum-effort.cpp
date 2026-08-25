f827249be1a023ae79949c29597ee93a2353c244
Anusha Azmeera <anushaazmeera006@gmail.com>
2026-08-25 22:32:11 +0530

    Time: 36 ms (95.56%), Space: 24.4 MB (77.26%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,116 +1,51 @@
-// class Solution {
-// public:
-//     int minimumEffortPath(vector<vector<int>>& heights) {
-
-//        int delx[]={-1,0,1,0};
-
-//         int dely[]={0,1,0,-1};
-//         int m= heights.size();
-//         int n = heights[0].size();
-//               int ans =0;
-//      // priority_queue<pair<int,int>,greater<pair<int,int>>,vector<pair<int,int>>>pq;
-//      priority_queue<pair<int,pair<int,int>>,
-//                vector<pair<int,pair<int,int>>>,
-//                greater<pair<int,pair<int,int>>>> pq;
-//          pq.push({0,{0,0}});
-        
-//          vector<vector<int>>dist(m,vector<int>(n,INT_MAX));
-//           dist[0][0]=0;
-//          while(!pq.empty()){
-//             auto it = pq.top();
-//             pq.pop();
-//             int r= it.second.first;
-//             int c= it.second.second;
-//             int diff= it.first;
-//              if(r==m-1&&c==n-1){
-//                 return diff;
-//             }       
-//         for(int i=0;i<4;i++){
-//             int nrow=r+delx[i];
-//             int ncol = c+dely[i];
-
-//             if(nrow>=0 && nrow < m&&ncol>=0 && ncol<n){
-//                 int actdiff= abs(heights[r][c]-heights[nrow][ncol]);
-                
-                   
-//                     // in box
-//                 int neweffort = max(ans,actdiff);
-//                 //we before replacing in dist  we wchcek current effort with the previous effort if any ,if current efforst is less we put there
-            
-//                 if(neweffort<dist[nrow][ncol]){
-//                     dist[nrow][ncol]=neweffort;
-//                     pq.push({neweffort,{nrow,ncol}});
-//                 }
-//             }
-            
-//         }
-
-//          }
-//          return 0;
-//     }
-// };
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
 
-        int delx[] = {-1,0,1,0};
-        int dely[] = {0,1,0,-1};
+       int delx[]={-1,0,1,0};
 
-        int m = heights.size();
+        int dely[]={0,1,0,-1};
+        int m= heights.size();
         int n = heights[0].size();
-
-        priority_queue<
-            pair<int,pair<int,int>>,
-            vector<pair<int,pair<int,int>>>,
-            greater<pair<int,pair<int,int>>>
-        > pq;
-
-        vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
-
-        dist[0][0] = 0;
-
-        pq.push({0,{0,0}});
-
-        while(!pq.empty()){
-
+              int ans =0;
+     // priority_queue<pair<int,int>,greater<pair<int,int>>,vector<pair<int,int>>>pq;
+     priority_queue<pair<int,pair<int,int>>,
+               vector<pair<int,pair<int,int>>>,
+               greater<pair<int,pair<int,int>>>> pq;
+         pq.push({0,{0,0}});
+        
+         vector<vector<int>>dist(m,vector<int>(n,INT_MAX));
+          dist[0][0]=0;
+         while(!pq.empty()){
             auto it = pq.top();
             pq.pop();
-
-            int diff = it.first;
-            int r = it.second.first;
-            int c = it.second.second;
-
-            if(r == m-1 && c == n-1){
+            int r= it.second.first;
+            int c= it.second.second;
+            int diff= it.first;
+             if(r==m-1&&c==n-1){
                 return diff;
-            }
+            }       
+        for(int i=0;i<4;i++){
+            int nrow=r+delx[i];
+            int ncol = c+dely[i];
 
-            for(int i = 0; i < 4; i++){
-
-                int nrow = r + delx[i];
-                int ncol = c + dely[i];
-
-                if(nrow >= 0 && nrow < m &&
-                   ncol >= 0 && ncol < n){
-
-                    int actdiff = abs(
-                        heights[r][c] - heights[nrow][ncol]
-                    );
-
-                    int neweffort = max(diff,actdiff);
-
-                    if(neweffort < dist[nrow][ncol]){
-
-                        dist[nrow][ncol] = neweffort;
-
-                        pq.push({
-                            neweffort,
-                            {nrow,ncol}
-                        });
-                    }
+            if(nrow>=0 && nrow < m&&ncol>=0 && ncol<n){
+                int actdiff= abs(heights[r][c]-heights[nrow][ncol]);
+                
+                   
+                    // in box
+                int neweffort = max(diff,actdiff);
+                //we before replacing in dist  we wchcek current effort with the previous effort if any ,if current efforst is less we put there
+            
+                if(neweffort<dist[nrow][ncol]){
+                    dist[nrow][ncol]=neweffort;
+                    pq.push({neweffort,{nrow,ncol}});
                 }
             }
+            
         }
 
-        return 0;
+         }
+         return 0;
     }
 };
