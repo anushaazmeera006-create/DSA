304557359e89957e00d36538bf40c68bf5b64eee
Anusha Azmeera <anushaazmeera006@gmail.com>
2026-09-07 19:51:34 +0530

    Time: 0 ms (100%), Space: 13.4 MB (34.28%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -19,39 +19,44 @@
 //       return ct;
 //     }
 // };
-// class Solution {
-// public:
-
-//     int findNumbers(vector<int>& nums) {
-//         int ct=0;
-//         int ans=0;
-//         vector<int>arr;
-//       for(int i=0;i<nums.size();i++){
-//         while(nums[i]!=0){
-//            arr.push_back(nums[i]%10);
-//               nums[i]=nums[i]/10;
-              
-//               }
-//               ct= arr.size();
-//               arr={0};
-//             if(ct%2==0) {
-//                 ans =ans+1;
-//             } 
-//       }
-//     return ans;
-//     }
-// };
 class Solution {
 public:
 
     int findNumbers(vector<int>& nums) {
-      int ct=0;
+        
+        int ans=0;
+        vector<int>arr;
       for(int i=0;i<nums.size();i++){
-        string s = to_string(nums[i]);
-        if(s.size()%2==0){
-            ct=ct+1;
+        int ct=0;
+        int x = nums[i];
+        if(x==0){
+            ct=1;
         }
+        while(x!=0){
+        
+              x=x/10;
+               ct=ct+1;
+              }
+              
+              
+            if(ct%2==0) {
+                ans =ans+1;
+            } 
       }
-    return ct;
+    return ans;
     }
 };
+// class Solution {
+// public:
+
+//     int findNumbers(vector<int>& nums) {
+//       int ct=0;
+//       for(int i=0;i<nums.size();i++){
+//         string s = to_string(nums[i]);
+//         if(s.size()%2==0){
+//             ct=ct+1;
+//         }
+//       }
+//     return ct;
+//     }
+// };
