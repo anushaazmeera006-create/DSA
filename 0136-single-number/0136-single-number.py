4644d300478376dc95b68819b591bf926a47fbd9
Anusha Azmeera <anushaazmeera006@gmail.com>
2026-09-10 14:19:26 +0530

    Time: 16 ms (17.33%), Space: 14.6 MB (12.04%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,15 +1,23 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        mp ={}
-        for x in nums: 
-          mp[x]=mp.get(x,0)+1
-        for x in mp:  
-            if mp[x]==1: 
-                ans= x
+# class Solution(object):
+#     def singleNumber(self, nums):
+#         mp ={}
+#         for x in nums: 
+#           mp[x]=mp.get(x,0)+1
+#         for x in mp:  
+#             if mp[x]==1: 
+#                 ans= x
 
-        return ans    
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+#         return ans    
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+from collections import Counter
+class Solution(object):
+    def singleNumber(self, nums):  
+        freq=Counter(nums)
+        for x in freq:
+            if freq[x]==1:
+               return x
+
         
